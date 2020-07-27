886d708dd16c32673ba7dd249d77a43cb9c54732
Andrew Hoefling <andrewh@fileonq.com>
2020-07-26 22:06:42 -0400

    Added basic Web API to get started

---
+++ Dnn.GitHubFeedback/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using DotNetNuke.Web.Api;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Dnn.GitHubFeedback.Controllers
+{
+    public class HomeController : DnnApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public HttpResponseMessage Ping()
+        {
+            var jsonContent = new StringContent("pong", Encoding.UTF8, "application/json");
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = jsonContent };
+        }
+    }
+}
