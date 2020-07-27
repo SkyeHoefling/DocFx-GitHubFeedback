2139fcacde3bfe52227b5c1631b9f4be46c9a158
Andrew Hoefling <andrewh@fileonq.com>
2020-07-26 22:12:39 -0400

    Initial commit

---
+++ Dnn.GitHubFeedback/Startup.cs
@@ -0,0 +1,14 @@
+﻿using DotNetNuke.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Dnn.GitHubFeedback
+{
+    public class Startup : IDnnStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // TODO - Register dependencies
+        }
+    }
+}
